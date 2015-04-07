792288e8f937af6ca0e3047dcb4d356e54436183
Johan Nestaas <johannestaas@gmail.com>
2015-04-07 15:51:41 -0700

    add http/https proxy config options

--- riskiq/cli/config.py
+++ riskiq/cli/config.py
@@ -1,27 +1,31 @@
 from riskiq.config import Config
 from riskiq.output import GenericOutput
-from optparse import OptionParser
+from argparse import ArgumentParser
 import sys
 
 
 def main():
-    usage = """%prog -t TOKEN -k KEY\n       %prog -p"""
-    parser = OptionParser(usage)
-    parser.add_option('-t', '--token', dest='token', default='', help='API token')
-    parser.add_option('-k', '--key', dest='key', default='', help='API private key')
-    parser.add_option('-p', '--print', action='store_true', dest='show_config',
-                      default=False, help='Show current API configuration')
-    options, args = parser.parse_args()
-    if options.show_config:
+    parser = ArgumentParser()
+    parser.add_argument('-t', '--token', help='API token')
+    parser.add_argument('-k', '--key', help='API private key')
+    parser.add_argument('--http-proxy', default='',
+        help='proxy to use for http requests')
+    parser.add_argument('--https-proxy', default='',
+        help='proxy to use for https requests')
+    parser.add_argument('-p', '--print', action='store_true', 
+        dest='show_config', help='Show current API configuration')
+    args = parser.parse_args()
+    if args.show_config:
         config = Config()
         show_config(config)
         sys.exit(0)
     config_options = {}
-    if not options.token or not options.key:
+    if not args.token or not args.key:
         parser.error("provide API token and secret key to configure client")
-    config_options['api_token'] = options.token
-    config_options['api_private_key'] = options.key
-
+    config_options['api_token'] = args.token
+    config_options['api_private_key'] = args.key
+    config_options['http_proxy'] = args.http_proxy
+    config_options['https_proxy'] = args.https_proxy
     config = Config(**config_options)
     show_config(config)
     sys.exit(0)
