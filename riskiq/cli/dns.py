c541da363e6c0a4edacd2523f11006482b937ee3
Darren Spruell <dspruell@riskiq.net>
2015-04-08 09:50:57 -0700

    Improved output formats for pDNS data. - Reduced default one-line output format to simple record data - Added verbose option to output complete record including first/lastseen and count - Removed lingering oneline option stuff from renderer - Removed trailing newlines from output of a few commands - Normalized template variable padding space (readability++)

--- riskiq/cli/dns.py
+++ riskiq/cli/dns.py
@@ -51,9 +51,11 @@ def main():
     name_p.add_argument('--json', '-j', action="store_true",
         help="Output as JSON")
     name_p.add_argument('--short', '-s', action="store_true",
-        help="Output as one line per entry in record.data")
+        help="Output newline-separated data only")
     name_p.add_argument('--text', '-T', action="store_true",
         help="Output full human readable text")
+    name_p.add_argument('--verbose', '-v', action="store_true",
+        help="Output verbose records with first/lastseen times and observed count")
 
     data_p = subs.add_parser('data')
     data_p.add_argument('addrs', nargs='+', help='Hostname or IP addresses')
@@ -61,9 +63,11 @@ def main():
     data_p.add_argument('--json', '-j', action="store_true",
         help="Output as JSON")
     data_p.add_argument('--short', '-s', action="store_true",
-        help="Output as one line per record.name")
+        help="Output newline-separated name only")
     data_p.add_argument('--text', '-T', action="store_true",
         help="Output full human readable text")
+    data_p.add_argument('--verbose', '-v', action="store_true",
+        help="Output verbose records with first/lastseen times and observed count")
 
     args = parser.parse_args()
 
@@ -83,10 +87,10 @@ def main():
         if args.json:
             print(json.dumps(data, indent=4))
         elif args.short and args.cmd == 'data':
-            print(renderer(data, 'dns/dns_data'))
+            print(renderer(data, 'dns/dns_data')),
         elif args.short and args.cmd == 'name':
-            print(renderer(data, 'dns/dns_name'))
+            print(renderer(data, 'dns/dns_name')),
         elif args.text:
-            print(renderer(data, 'dns/dns'))
+            print(renderer(data, 'dns/dns')),
         elif data:
-            print(renderer(data, 'dns/dns_oneline'))
+            print(renderer(data, 'dns/dns_oneline', verbose=args.verbose)),
--- riskiq/render.py
+++ riskiq/render.py
@@ -4,15 +4,13 @@ from jinja2 import Template
 
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 
-def renderer(data, datatype, oneline=False):
+def renderer(data, datatype, verbose=False):
     """
     Render data of type datatype
     Example datatype: "blacklist/lookup"
     """
     template_path = os.path.join(TEMPLATE_DIR, datatype)
-    if oneline:
-        template_path += '_oneline'
     with open(template_path) as temp_f:
         template_text = temp_f.read()
     template = Template(template_text)
-    return template.render(data=data).encode('utf-8')
+    return template.render(data=data, verbose=verbose).encode('utf-8')
