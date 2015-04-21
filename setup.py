523117998d06132a253f23a353ecd5f04e7fb7eb
Johan Nestaas <johannestaas@gmail.com>
2015-04-21 10:57:17 -0700

    forgot comma

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires = ['requests', 'jinja2'],
     extras_require = {
         'stix': ['stix'],
-    }
+    },
     long_description=read('README.rst'),
     classifiers=[
         'Development Status :: 3 - Alpha',
