4a2f29ee7922f388ed9684878cce57071bb4abe7
Johan Nestaas <johannestaas@gmail.com>
2015-05-05 07:16:54 -0700

    More sane configuration setup in riq-config {setup,show}

--- riskiq/cli/config.py
+++ riskiq/cli/config.py
@@ -1,46 +1,42 @@
+#!/usr/bin/env python
 from riskiq.config import Config
 from riskiq.output import GenericOutput
 from argparse import ArgumentParser
 import sys
 
+def show_config(config):
+    print("\nCurrent Configuration:\n")
+    for k, v in sorted(config.config.items()):
+        print("{0:15}: {1}".format(k, v))
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('-t', '--token', default='',
-        help='API token')
-    parser.add_argument('-k', '--key', default='',
-        help='API private key')
-    parser.add_argument('--http-proxy', default='',
+    subs = parser.add_subparsers(dest='cmd')
+
+    setup_parser = subs.add_parser('setup')
+    setup_parser.add_argument('token', help='API token')
+    setup_parser.add_argument('private_key', help='API private key')
+    setup_parser.add_argument('--http-proxy', '--http', default='',
         help='proxy to use for http requests')
-    parser.add_argument('--https-proxy', default='',
+    setup_parser.add_argument('--https-proxy', '--https', default='',
         help='proxy to use for https requests')
-    parser.add_argument('-p', '--print', action='store_true', 
-        dest='show_config', help='Show current API configuration')
+
+    show_parser = subs.add_parser('show',
+        help='show current API configuration')
     args = parser.parse_args()
-    if args.show_config:
+
+    if args.cmd == 'show':
         config = Config()
         show_config(config)
-        sys.exit(0)
-    config_options = {}
-    if not (args.token and args.key):
-        print >> sys.stderr, (
-            'warning: missing token or private key'
-        )
-        sys.exit(1)
-    config_options['api_token'] = args.token
-    config_options['api_private_key'] = args.key
-    config_options['http_proxy'] = args.http_proxy
-    config_options['https_proxy'] = args.https_proxy
-    config = Config(**config_options)
-    show_config(config)
-    sys.exit(0)
+    elif args.cmd == 'setup':
+        config_options = {}
+        config_options['api_token'] = args.token
+        config_options['api_private_key'] = args.private_key
+        config_options['http_proxy'] = args.http_proxy
+        config_options['https_proxy'] = args.https_proxy
+        config = Config(**config_options)
+        show_config(config)
 
-
-def show_config(config):
-    print "\nCurrent Configuration:\n"
-    for k, v in sorted(config.config.items()):
-        print "%15s: %s" % (k, v)
 
 if __name__ == '__main__':
     main()
-
