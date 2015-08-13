592684cfecc9c5f77184c0450f32c42026a2b88b
Johan Nestaas <johannestaas@gmail.com>
2015-08-13 10:24:10 -0700

    add --start-index and --max-results to cli for bl incident

--- riskiq/cli/blacklist/incident.py
+++ riskiq/cli/blacklist/incident.py
@@ -9,11 +9,19 @@ def add_parser(subs):
         'by given URL/host/domain')
     parser.add_argument('urls', nargs='+', metavar='URL',
         help='URL/host/domain for which to query')
+    parser.add_argument('--start-index', type=int,
+        help='start index, for pagination (default 0)')
+    parser.add_argument('--max-results', type=int,
+        help='max results to return (default 10)')
     add_render_args(parser)
 
 @templated('blacklist/incident', yielding=True)
 def run(client, args, kwargs):
     urls = stdin(args.urls)
+    blkwargs = {}
+    for url_param in ('start_index', 'max_results'):
+        if getattr(args, url_param) is not None:
+            blkwargs[url_param] = getattr(args, url_param)
     for url in urls:
-        data = client.get_blacklist_incident(url)
+        data = client.get_blacklist_incident(url, **blkwargs)
         yield data, kwargs
