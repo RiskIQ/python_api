62b595341d437249c02835a891c32e7f5063dd75
Johan Nestaas <johannestaas@gmail.com>
2015-08-20 15:09:53 -0700

    test bl_list

--- riskiq/cli/tests.py
+++ riskiq/cli/tests.py
@@ -13,13 +13,28 @@ ROOT_DIR = os.path.dirname(__file__)
 def load_data(filename):
     path = os.path.join(ROOT_DIR, 'testdata', filename)
     with open(path) as f:
-        return json.load(f)
+        data = json.load(f)
+    return data
+
+def read(filename):
+    path = os.path.join(ROOT_DIR, 'testdata', 'output', filename)
+    with open(path) as f:
+        return f.read().strip()
 
 RETURN_VALUES = {
     'blacklist_incident': load_data('zief.pl.json'),
+    'blacklist_list': load_data('bl_list.json'),
 }
 
-GLOBALS = {}
+ASSERT_VALUES = {
+    'blacklist_list.oneline': read('bl_list.oneline'),
+    'blacklist_incident.oneline': read('bl_incident.oneline'),
+}
+
+MOCKED_OBJECTS = {}
+
+def assert_val(x):
+    return ASSERT_VALUES[x]
 
 def ret_val(x):
     return RETURN_VALUES[x]
@@ -27,12 +42,13 @@ def ret_val(x):
 def setup_blacklist():
     client = MagicMock()
     client.get_blacklist_incident.return_value = ret_val('blacklist_incident')
+    client.get_blacklist_list.return_value = ret_val('blacklist_list')
     args = MagicMock()
     args.urls = ['zief.pl']
-    stdout = sys.stdout
-    GLOBALS['client'] = client
-    GLOBALS['args'] = args
-    GLOBALS['stdout'] = stdout
+    args.start_index = None
+    args.max_results = None
+    MOCKED_OBJECTS['client'] = client
+    MOCKED_OBJECTS['args'] = args
 
 def teardown_blacklist():
     pass
@@ -40,6 +56,19 @@ def teardown_blacklist():
 @with_setup(setup_blacklist, teardown_blacklist)
 def test_bl_incident_oneline():
     from riskiq.cli.blacklist.incident import run
-    client, args = GLOBALS['client'], GLOBALS['args']
-    kwargs = {'oneline': True}
-    run(client, args, kwargs)
+    client, args = MOCKED_OBJECTS['client'], MOCKED_OBJECTS['args']
+    output = run(client, args, {'oneline': True, 'return_output': True})
+    good = assert_val('blacklist_incident.oneline')
+    print('\ngood: {}'.format(good))
+    print('bad:  {}'.format(output))
+    assert output == good
+
+@with_setup(setup_blacklist, teardown_blacklist)
+def test_bl_list_oneline():
+    from riskiq.cli.blacklist.bl_list import run
+    client, args = MOCKED_OBJECTS['client'], MOCKED_OBJECTS['args']
+    output = run(client, args, {'oneline': True, 'filter': None, 'return_output': True})
+    good = assert_val('blacklist_list.oneline')
+    print('\ngood: {}'.format(good))
+    print('bad:  {}'.format(output))
+    assert output == good
