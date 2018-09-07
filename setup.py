25bca408c15a23428ce7d891e33161aaf4a968a7
Joseph Laycock <41761622+jlaycock-riq@users.noreply.github.com>
2018-09-06 19:27:54 -0700

    Added new version details

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = 'riskiq',
-    version = '0.4.12',
+    version = '0.4.13',
     description = 'client for RiskIQ REST API',
     url = "https://github.com/riskiq/python_api",
     keywords = 'riskiq API REST',
